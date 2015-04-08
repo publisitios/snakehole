d89dd34106eaee6f824ba5eda78add408668192b
Xavier Fernandez <xavier@crservers.com>
2015-04-08 10:49:40 -0400

    this is the second commit

--- snakehole.js
+++ snakehole.js
@@ -6,4 +6,22 @@ var app = express();
 var client_ID = "xaviercr"
 var redirect_URI = "http://xavierf.software/" 
 
-var URL = "https://api.instagram.com/oauth/authorize/?client_id="+CLIENT-ID+"&redirect_uri="+REDIRECT-URI+"&response_type=code"
+var URL = "https://api.instagram.com/oauth/authorize/?client_id="+CLIENT_ID+"&redirect_uri="+REDIRECT_URI+"&response_type=code";
+
+
+
+app.get('/', function (req, res) {
+  res.send('WELCOME TO SNAKE HOLE!');
+});
+
+
+
+
+
+
+// start the HTTP server
+var server = app.listen(80, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('SnakeHole app listening at http://%s:%s', host, port);
+});
