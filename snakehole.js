2ba85da84decd4b3900df378e30f27e377ba289b
Xavier Fernandez <xavier@crservers.com>
2015-04-08 10:42:56 -0400

    initial commit

---
+++ snakehole.js
@@ -0,0 +1,9 @@
+// dependencies
+var express = require("express");
+var app = express();
+
+// global variables
+var client_ID = "xaviercr"
+var redirect_URI = "http://xavierf.software/" 
+
+var URL = "https://api.instagram.com/oauth/authorize/?client_id="+CLIENT-ID+"&redirect_uri="+REDIRECT-URI+"&response_type=code"
